a4bf49ad758b18dd8935a027bee68e7d386b70c5
hcz <h.cz@qq.com>
2014-08-07 00:22:11 +0800

    add gc

--- include/cotl.hpp
+++ include/cotl.hpp
@@ -1,6 +1,8 @@
 #ifndef _COTL_HPP
 #define _COTL_HPP
 
+#include "cotl_gc.hpp"
+
 namespace cotl {
     long long ago;
 }
---
+++ include/cotl_gc.hpp
@@ -0,0 +1,9 @@
+#ifndef _COTL_GC_HPP
+#define _COTL_GC_HPP
+
+// use bdwgc
+#include "../gc/include/gc.h"
+#include "../gc/include/gc_cpp.h"
+#include "../gc/include/gc_allocator.h"
+
+#endif
