5cccafd996d491e7a29dd2ddb0ed807fc04e5fac
hcz <h.cz@qq.com>
2014-08-11 23:22:00 +0800

    add config file and apply libgc from OS

--- include/cotl_gc.hpp
+++ include/cotl_gc.hpp
@@ -1,9 +1,18 @@
 #ifndef _COTL_GC_HPP
 #define _COTL_GC_HPP
 
-// use bdwgc
-#include "../gc/include/gc.h"
-#include "../gc/include/gc_cpp.h"
-#include "../gc/include/gc_allocator.h"
+#ifdef _COTL_USE_GLOBAL_GC
+    // use libgc from OS
+    #include <gc.h>
+    #include <gc_cpp.h>
+    // #include <gc_allocator.h>
+
+#else
+    // use local bdwgc
+    #include "../gc/include/gc.h"
+    #include "../gc/include/gc_cpp.h"
+    #include "../gc/include/gc_allocator.h"
+
+#endif
 
 #endif
