4bb99fbea69a32067c1f75a8de0366931cb3bfc0
hcz <h.cz@qq.com>
2014-09-05 18:57:45 +0800

    fix

--- include/cotl_gc.hpp
+++ include/cotl_gc.hpp
@@ -22,32 +22,36 @@ namespace cotl {
 #ifdef _COTL_USE_GC
     #define _COTL_MALLOC(size) GC_MALLOC(size)
     #define _COTL_FREE(ptr) GC_FREE(ptr)
-    #define _COTL_RAWNEW(obj) (new (UseGC) obj)
     #define _COTL_MEMINIT() GC_INIT()
 
     using MemBase = gc_cleanup;
     template <class T>
     #ifdef _COTL_USE_WEAK_ALLOCATOR
         using MemAlloc = typename ::traceable_allocator<T>;
+        #define _COTL_RAWNEW(obj) (new obj)
+        #define _COTL_RAWDEL(obj) (delete obj)
     #else
         using MemAlloc = typename ::gc_allocator<T>;
+        #define _COTL_RAWNEW(obj) (new (UseGC) obj)
+        #define _COTL_RAWDEL(obj) ((void) obj)
     #endif
 #else
     #define _COTL_MALLOC(size) malloc(size)
     #define _COTL_FREE(ptr) free(ptr)
-    #define _COTL_RAWNEW(obj) (new obj)
     #define _COTL_MEMINIT() do {} while (false)
 
     using MemBase = class {};
     template <class T>
     using MemAlloc = typename std::allocator<T>;
+
+    #define _COTL_RAWNEW(obj) (new obj)
 #endif
 
 template <class T>
 class MemDel {
 public:
     inline void operator()(T *ptr) const {
-        _COTL_FREE(ptr);
+        _COTL_RAWDEL(ptr);
     }
 };
 
