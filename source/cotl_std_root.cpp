c631237403731fb357625785694b73f8b2ac8bea
hcz <h.cz@qq.com>
2014-09-23 13:46:23 +0800

    apply math lib and simplify cotl_std_root.cpp

--- source/cotl_std_root.cpp
+++ source/cotl_std_root.cpp
@@ -17,25 +17,21 @@ _COTL_FUNC_BEGIN
 
     const map_t &map_data = map_p->get();
 
-    {
-        PMaybe map1(nullptr);
-        libGet(id_type, lib, map1);
-
-        _COTL_CHECK_TYPE(map1, cotl::Map, id_map);
-
-        const map_t &map1_data = map1_p->get();
-        map_data->insert(map1_data->begin(), map1_data->end());
+    #define _COTL_ADD_LIB_TO_STD(id) \
+    {\
+        PMaybe map1(nullptr);\
+        libGet(id, lib, map1);\
+        _COTL_CHECK_TYPE(map1, cotl::Map, id_map);\
+        const map_t &map1_data = map1_p->get();\
+        map_data->insert(map1_data->begin(), map1_data->end());\
     }
 
-    {
-        PMaybe map1(nullptr);
-        libGet(id_runtime, lib, map1);
+    _COTL_ADD_LIB_TO_STD(id_type)
+    _COTL_ADD_LIB_TO_STD(id_runtime)
+    _COTL_ADD_LIB_TO_STD(id_math)
+    // ...
 
-        _COTL_CHECK_TYPE(map1, cotl::Map, id_map);
-
-        const map_t &map1_data = map1_p->get();
-        map_data->insert(map1_data->begin(), map1_data->end());
-    }
+    #undef _COTL_ADD_LIB_TO_STD
 _COTL_FUNC_END
 
 long long ago = addInitializer(init, init_lib_after);
