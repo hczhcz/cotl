86db499b5b25280b6f5df94cb071b44c127ee2dc
hcz <h.cz@qq.com>
2014-09-28 12:35:21 +0800

    add cotl_std_cast.cpp

---
+++ source/cotl_std_cast.cpp
@@ -0,0 +1,40 @@
+#include "../cotl"
+
+namespace cotlstd {
+
+// arg: self, caller, lib, tunnel
+
+namespace {
+
+/*
+real_t mul(int_t val) {
+    return val;
+}
+
+str_t mul(int_t val) {
+    return val;
+}
+
+str_t mul(int_t val) {
+    return val;
+}
+
+_COTL_FUNC_T(init)
+_COTL_FUNC_BEGIN
+    _COTL_CHECK_TUNNEL(false);
+
+    PMaybe tunnel1(
+        _libmap(
+            id_???, _libfunc(std???),
+        )
+    );
+
+    libSet(id_cast, lib, tunnel1);
+_COTL_FUNC_END
+
+long long ago = addInitializer(init, init_???);
+*/
+
+}
+
+}
